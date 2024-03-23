fb0f75751553cc470c60dd2a2e8a1325e1b6c7e4
VictorVigara <victor@meilirobots.com>
2024-03-23 18:29:10 +0100

    simulation example with rviz

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'px4_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('resource/*rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='victor',
+    maintainer_email='vigaravictor99@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'offboard_control = px4_control.offboard_control:main',
+            'visualizer = px4_control.visualizer:main', 
+            'processes = px4_control.processes:main'
+        ],
+    },
+)
